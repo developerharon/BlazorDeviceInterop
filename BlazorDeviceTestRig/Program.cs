e25ed1ea0de66128ec7e1ee0a060c1fe551d0015
Haron Kipkorir <ronoharonkip@gmail.com>
2023-06-19 17:29:46 +0300

    Changed Program.cs to use top level statements

--- BlazorDeviceTestRig/Program.cs
+++ BlazorDeviceTestRig/Program.cs
@@ -1,24 +1,17 @@
 using System;
 using System.Net.Http;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using Microsoft.Extensions.DependencyInjection;
 using Darnton.Blazor.DeviceInterop.Geolocation;
+using BlazorDeviceTestRig;
+using Microsoft.AspNetCore.Components.Web;
 
-namespace BlazorDeviceTestRig
-{
-    public class Program
-    {
-        public static async Task Main(string[] args)
-        {
-            var builder = WebAssemblyHostBuilder.CreateDefault(args);
-            builder.RootComponents.Add<App>("app");
-            
-            builder.Services.AddScoped<IGeolocationService, GeolocationService>();
+var builder = WebAssemblyHostBuilder.CreateDefault(args);
+builder.RootComponents.Add<App>("app");
+builder.RootComponents.Add<HeadOutlet>("head::after");
 
-            builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+builder.Services.AddScoped<IGeolocationService, GeolocationService>();
 
-            await builder.Build().RunAsync();
-        }
-    }
-}
+builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+
+await builder.Build().RunAsync();
